8ca8154d709cf2db1674c5a9bad454857941d0ba
hirespacedev@gmail.com <hirespacedev@gmail.com>
2013-10-16 11:02:33 +0100

    Bootstrap 3 datatables v1

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  summary: "DataTable - jQuery plugin for sortable, pagable data"
+});
+
+Package.on_use(function (api) {
+  api.use('jquery', 'client');
+  api.add_files([
+    'lib/jquery.dataTables.js',
+	'lib/DT_bootstrap.js',
+    'lib/images/back_disabled.png',
+    'lib/images/back_enabled.png',
+	'lib/images/back_enabled_hover.png',
+	'lib/images/forward_disabled.png',
+	'lib/images/forward_enabled.png',
+	'lib/images/forward_enabled_hover.png',
+	'lib/images/sort_asc.png',
+	'lib/images/sort_asc_disabled.png',
+	'lib/images/sort_both.png',
+	'lib/images/sort_desc.png',
+	'lib/images/sort_desc_disabled.png',
+  ], 'client'
+  );
+});
